462b3315165778d6c31c5593db90b483bdda25a0
Someones_Git7 <29richmoca@gowcs.net>
2026-01-29 09:21:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-function hit() {
-  if (!gameInProgress) return;
 
-  dealCard(playerCards, playerCardDisplays);
-
-  var playerValue = getHandValue(playerCards);
-  updatePlayerValue();
-
-  if (playerValue > 21) {
-    document.getElementById("message").innerHTML = "You bust! You lose $" + currentBet + ".";
-    money -= currentBet;  // <-- ADDED THIS LINE
-    endGame(false);
-  } else if (playerValue == 21) {
-    stand();
-  }
-}
